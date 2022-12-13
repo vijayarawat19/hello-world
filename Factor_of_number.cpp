361c1717434d27f6a1df1d970562fea559b94e19
vijayarawat19 <70505056+vijayarawat19@users.noreply.github.com>
2022-12-13 08:14:51 +0530

    Create Factor_of_number.cpp

---
+++ Factor_of_number.cpp
@@ -0,0 +1,19 @@
+//Factors of number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,m,num;
+    cin>>num;
+    i=1;
+    m=1;
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0){
+            //cout<<i;
+            cout<<i;
+        }
+    }
+    
+    
+}
